3f7566e0789e2e09b548b67203dee3981adf48ea
Chimera <j.calderon3@gmail.com>
2021-03-22 18:33:05 -0400

    directory clean-up

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div>
+
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
