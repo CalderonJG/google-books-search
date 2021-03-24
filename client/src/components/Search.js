6778a8886adb5d87626bdea13b26e9a949a6b79a
Chimera <j.calderon3@gmail.com>
2021-03-23 20:12:42 -0400

    book model updated; Modify/Search components added

---
+++ client/src/components/Search.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function SearchInput(props) {
+    return (
+        <div className="row d-flex justify-content-center">
+            <div className="search-field">
+                <div>
+                    <form className="search-bar form-inline">
+                      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"  {...props}/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function SearchBtn (props) {
+
+    return (
+        <div className="row d-flex justify-content-center">
+            <button {...props} className="searchBtn btn btn-outline-light my-2 my-sm-0 " style= {{marginBottom: "16%"}}>Search</button>
+        </div>
+
+    )
+}
